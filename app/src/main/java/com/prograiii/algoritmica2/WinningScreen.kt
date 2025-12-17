e446fd07f1a144af3af78caf6f0d535f60934f29
TrynaFindMimi <nahomihumerez.4s@gmail.com>
2025-12-17 18:10:48 -0400

    Implementacion nuevas pantallas

---
+++ app/src/main/java/com/prograiii/algoritmica2/WinningScreen.kt
@@ -0,0 +1,34 @@
+package com.prograiii.algoritmica2
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.prograiii.algoritmica2.databinding.ActivityWinningScreenBinding
+
+class WinningScreen : AppCompatActivity() {
+    private lateinit var binding: ActivityWinningScreenBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        binding = ActivityWinningScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        binding.btnNewGame.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK
+            startActivity(intent)
+            finish()
+        }
+    }
+}
